4a9268c57a8ec6007dba9f8e93a39f1029d73cc7
Aldycky Bagus W <bagusaldycky@gmail.com>
2022-03-18 18:17:18 +0700

    integrasi service courses dengan API gateway bagian API image-courses, lessons, my-courses, dan reviews

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const coursesRouter = require("./routes/courses");
 const chaptersRouter = require("./routes/chapters");
+const lessonsRouter = require("./routes/lessons");
 const mediaRouter = require("./routes/media");
 const ordersRouter = require("./routes/orders");
 const paymentsRouter = require("./routes/payments");
 const refreshTokensRouter = require("./routes/refreshTokens");
-const mentorsRouter = require('./routes/mentors');
+const mentorsRouter = require("./routes/mentors");
+const imageCoursesRouter = require("./routes/imageCourses");
+const myCoursesRouter = require("./routes/myCourses");
+const reviewsRouter = require("./routes/reviews");
 
 const verifyToken = require("./middleware/verifyToken");
 
@@ -28,10 +32,14 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/courses", coursesRouter);
 app.use("/chapters", verifyToken, chaptersRouter);
+app.use("/lessons", verifyToken, lessonsRouter);
 app.use("/media", mediaRouter);
 app.use("/orders", ordersRouter);
 app.use("/payments", paymentsRouter);
 app.use("/refresh-tokens", refreshTokensRouter);
-app.use('/mentors', verifyToken, mentorsRouter);
+app.use("/mentors", verifyToken, mentorsRouter);
+app.use("/image-courses", verifyToken, imageCoursesRouter);
+app.use("/my-courses", verifyToken, myCoursesRouter);
+app.use("/reviews", verifyToken, reviewsRouter);
 
 module.exports = app;
