9790d796ee5a5bbad8345a1b0ca5ac6d3b1b0c4d
aldyckybw <bagusaldycky@gmail.com>
2023-02-11 13:04:12 +0700

    added cors for memberpage localhost:3030

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -23,6 +24,11 @@ const can = require("./middleware/permission");
 
 const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:3030",
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
